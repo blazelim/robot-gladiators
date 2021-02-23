12b6a1b44a4bebc29eea005117cf355473f1289a
Blaze Lim <blazebentleycolim@gmail.com>
2021-02-23 12:57:12 -0800

    added console variables

---
+++ assets/js/game.js
@@ -0,0 +1,29 @@
+/*
+
+Comments also work this way
+
+*/
+
+//adding prompt to inquire robot name
+var playerName = window.prompt ("what is your robot's name?");
+
+// Note the lack of quotation marks around the playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// this creates a function named "fight"
+
+
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
